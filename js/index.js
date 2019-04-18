166b6bfa6287d3a5103899594ac309c27671b531
Jose Jimenez-Olivas <josejimenezjr18@gmail.com>
2019-04-18 13:09:26 -0700

    commented out timeout

--- js/index.js
+++ js/index.js
@@ -2,5 +2,5 @@ let one_second = 1000;
 window.onload = delay( 30 * one_second );
 
 function delay( ms ) {
-  setTimeout( function(){ alert("delay is done" + ms) }, ms );
+  //setTimeout( function(){ alert("delay is done" + ms) }, ms );
 }
